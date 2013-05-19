325e8b90ffc86c4c259c5d4bb973ee243fea382c
Matt Rogers <mattrogers@sbcglobal.net>
2013-05-18 21:24:10 -0500

    Switch to SVG for the graphs.
    Provides more flexibility

--- d3/bar-chart-part-1/barchart.js
+++ d3/bar-chart-part-1/barchart.js
@@ -1,13 +1,18 @@
 var data = [4,8,15,16,23,42]
 
-var chart = d3.select("body").append("div").attr("class", "chart");
+var chart = d3.select("body").append("svg")
+  .attr("class", "chart")
+  .attr("width", "420")
+  .attr("height", 20 * data.length);
+
 var x = d3.scale.linear()
   .domain([0, d3.max(data)])
-  .range(["0px", "420px"]);
+  .range([0, 420]);
 
 
-chart.selectAll("div")
+chart.selectAll("rect")
   .data(data)
-  .enter().append("div")
-    .style("width", x)
-    .text(String);
+  .enter().append("rect")
+    .attr("y", function(d, i) { return i * 20; })
+    .attr("width", x)
+    .attr("height", 20);
