74852f1d2ad8a9c1e9293da07e54faf064c2f0b7
Matt Rogers <mattrogers@sbcglobal.net>
2013-05-18 22:00:30 -0500

    Add some padding to the chart

--- d3/bar-chart-part-1/barchart.js
+++ d3/bar-chart-part-1/barchart.js
@@ -2,8 +2,11 @@ var data = [4,8,15,16,23,42]
 
 var chart = d3.select("body").append("svg")
   .attr("class", "chart")
-  .attr("width", "420")
-  .attr("height", 20 * data.length);
+  .attr("width", 440)
+  .attr("height", 140)
+  .append("g")
+    .attr("transform", "translate(10,15)");
+
 
 var x = d3.scale.linear()
   .domain([0, d3.max(data)])
@@ -20,3 +23,13 @@ chart.selectAll("rect")
     .attr("y", y)
     .attr("width", x)
     .attr("height", y.rangeBand());
+
+chart.selectAll("text")
+  .data(data)
+  .enter().append("text")
+    .attr("x", x)
+    .attr("y", function(d) { return y(d) + y.rangeBand() / 2; })
+    .attr("dx", -3)
+    .attr("dy", ".35em")
+    .attr("text-anchor", "end")
+    .text(String);
