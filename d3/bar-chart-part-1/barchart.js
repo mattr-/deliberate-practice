cd63618701789dfa9e2d250152461aed145453a4
Matt Rogers <mattrogers@sbcglobal.net>
2013-05-18 21:19:32 -0500

    Simplify the chart definition based on the tutorial

--- d3/bar-chart-part-1/barchart.js
+++ d3/bar-chart-part-1/barchart.js
@@ -1,9 +1,13 @@
 var data = [4,8,15,16,23,42]
 
 var chart = d3.select("body").append("div").attr("class", "chart");
+var x = d3.scale.linear()
+  .domain([0, d3.max(data)])
+  .range(["0px", "420px"]);
+
 
 chart.selectAll("div")
   .data(data)
   .enter().append("div")
-    .style("width", function(d) { return d * 10 + "px"; })
-    .text(function(d) { return d; });
+    .style("width", x)
+    .text(String);
