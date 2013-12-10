8cb8e2008f6b9ad43b41b1618784e1dec0e305ee
Matt Rogers <mattrogers@sbcglobal.net>
2013-12-09 22:02:13 -0600

    Start working on a StringCount exercise.

---
+++ cocoa-programming-osx-fourth-edition/StringCount/StringCount/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  StringCount
+//
+//  Created by Matt Rogers on 11/30/13.
+//  Copyright (c) 2013 Matt Rogers. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
