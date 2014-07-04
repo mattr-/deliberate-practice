9c245ff3fac11edfafb5b4d8db044423ea2adec5
Matt Rogers <mattrogers@sbcglobal.net>
2014-07-04 13:08:07 -0500

    Add a StringCounter exercise
    This is an exercise from the end of chapter 5 where you're asked to
    create an application from scratch after learning about outlets,
    actions, and making connections.

---
+++ cocoa-programming-osx-fourth-edition/StringCounter/StringCounter/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  StringCounter
+//
+//  Created by Matt Rogers on 7/4/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
