8f6af7353d83f2958d6dccf82c3751d8ceaa7943
Matt Rogers <mattrogers@sbcglobal.net>
2014-07-04 14:39:13 -0500

    Implement SpeakLine enhancements from Chapters 5 and 6
    Button enabling and disabling, delegates, and voice selection

--- cocoa-programming-osx-fourth-edition/SpeakLine/SpeakLine/SpeakLineAppDelegate.h
+++ cocoa-programming-osx-fourth-edition/SpeakLine/SpeakLine/SpeakLineAppDelegate.h
@@ -8,13 +8,19 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface SpeakLineAppDelegate : NSObject <NSApplicationDelegate>
+@interface SpeakLineAppDelegate :
+    NSObject <NSApplicationDelegate, NSSpeechSynthesizerDelegate>
 {
+    NSArray *_voices;
     NSSpeechSynthesizer *_speechSynth;
 }
 
 @property (assign) IBOutlet NSWindow *window;
 @property (weak) IBOutlet NSTextField *textField;
+@property (weak) IBOutlet NSButton *startButton;
+@property (weak) IBOutlet NSButton *stopButton;
+@property (weak) IBOutlet NSTableView *tableView;
+
 
 - (IBAction)stopIt:(id)sender;
 - (IBAction)sayIt:(id)sender;
--- cocoa-programming-osx-fourth-edition/SpeakLine/SpeakLine/SpeakLineAppDelegate.m
+++ cocoa-programming-osx-fourth-edition/SpeakLine/SpeakLine/SpeakLineAppDelegate.m
@@ -16,6 +16,9 @@
     if (self) {
         NSLog(@"init");
         _speechSynth = [[NSSpeechSynthesizer alloc] initWithVoice:nil];
+        [_speechSynth setDelegate:self];
+        
+        _voices = [NSSpeechSynthesizer availableVoices]; 
     }
     
     return self;
@@ -24,6 +27,15 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+}
+
+- (void)awakeFromNib
+{
+    NSString *defaultVoice = [NSSpeechSynthesizer defaultVoice];
+    NSInteger defaultRow = [_voices indexOfObject:defaultVoice];
+    NSIndexSet *indices = [NSIndexSet indexSetWithIndex:defaultRow];
+    [_tableView selectRowIndexes:indices byExtendingSelection:NO];
+    [_tableView scrollRowToVisible:defaultRow];    
 }
 
 - (IBAction)stopIt:(id)sender {
@@ -39,7 +51,43 @@
         return;
     }
     
-    [_speechSynth startSpeakingString:string];
     
+    [_speechSynth startSpeakingString:string];
+    [_stopButton setEnabled:YES];
+    [_startButton setEnabled:NO];
+    [_tableView setEnabled:NO];
+    
+}
+
+- (void)speechSynthesizer:(NSSpeechSynthesizer *)sender didFinishSpeaking:(BOOL)finishedSpeaking
+{
+    NSLog(@"Finished Speaking: %d", finishedSpeaking);
+    [_stopButton setEnabled:NO];
+    [_startButton setEnabled:YES];
+    [_tableView setEnabled:YES];
+}
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
+{
+    return (NSInteger)[_voices count];
+}
+
+- (id)tableView:(NSTableView *)tv
+objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row
+{
+    NSString *voice = [_voices objectAtIndex:row];
+    NSDictionary *dict = [NSSpeechSynthesizer attributesForVoice:voice];
+    return [dict objectForKey:NSVoiceName];
+}
+
+- (void)tableViewSelectionDidChange:(NSNotification *)notification
+{
+    NSInteger row = [_tableView selectedRow];
+    if (row == -1) {
+        return;
+    }
+    
+    NSString *selectedVoice = [_voices objectAtIndex:row];
+    [_speechSynth setVoice:selectedVoice];
 }
 @end
