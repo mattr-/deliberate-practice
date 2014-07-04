cd4c8b58195b7ca9380617ba951bbd555e430161
Matt Rogers <mattrogers@sbcglobal.net>
2014-07-04 13:09:23 -0500

    Implement the full SpeakLine example

--- cocoa-programming-osx-fourth-edition/SpeakLine/SpeakLine/SpeakLineAppDelegate.h
+++ cocoa-programming-osx-fourth-edition/SpeakLine/SpeakLine/SpeakLineAppDelegate.h
@@ -9,8 +9,12 @@
 #import <Cocoa/Cocoa.h>
 
 @interface SpeakLineAppDelegate : NSObject <NSApplicationDelegate>
+{
+    NSSpeechSynthesizer *_speechSynth;
+}
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *textField;
 
 - (IBAction)stopIt:(id)sender;
 - (IBAction)sayIt:(id)sender;
--- cocoa-programming-osx-fourth-edition/SpeakLine/SpeakLine/SpeakLineAppDelegate.m
+++ cocoa-programming-osx-fourth-edition/SpeakLine/SpeakLine/SpeakLineAppDelegate.m
@@ -10,14 +10,36 @@
 
 @implementation SpeakLineAppDelegate
 
+-(id)init
+{
+    self = [super init];
+    if (self) {
+        NSLog(@"init");
+        _speechSynth = [[NSSpeechSynthesizer alloc] initWithVoice:nil];
+    }
+    
+    return self;
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
 }
 
 - (IBAction)stopIt:(id)sender {
+    NSLog(@"Silencing");
+    [_speechSynth stopSpeaking];
 }
 
 - (IBAction)sayIt:(id)sender {
+    NSString *string = [_textField stringValue];
+    
+    if ([string length] == 0) {
+        NSLog(@"String from %@ is zero length", _textField);
+        return;
+    }
+    
+    [_speechSynth startSpeakingString:string];
+    
 }
 @end
