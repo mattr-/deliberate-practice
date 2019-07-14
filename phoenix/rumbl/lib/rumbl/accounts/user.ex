951d8fceb1465007cab1b6219be5248d1b4a63cd
Matt Rogers <mattr-@github.com>
2019-07-14 14:55:48 -0500

    Progress through the end of Chapter 4
    This is all the code that's been written for our example application in
    Programming Phoenix through the end of Chapter 4. We have a database for
    storing users now! 🎉

--- phoenix/rumbl/lib/rumbl/accounts/user.ex
+++ phoenix/rumbl/lib/rumbl/accounts/user.ex
@@ -2,5 +2,20 @@ defmodule Rumbl.Accounts.User do
   @moduledoc """
   The User struct used in Rumbl.Accounts
   """
-  defstruct [:id, :name, :username]
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+
+    timestamps()
+  end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :username])
+    |> validate_required([:name, :username])
+    |> validate_length(:username, min: 1, max: 20)
+  end
 end
--- phoenix/rumbl/lib/rumbl_web/router.ex
+++ phoenix/rumbl/lib/rumbl_web/router.ex
@@ -17,6 +17,7 @@ defmodule RumblWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.
