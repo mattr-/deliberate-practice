df0be934b6a82e1cd049420d5ca9073c9f970468
Matt Rogers <codemattr@gmail.com>
2025-01-29 15:51:05 -0600

    chore: say goodbye to everything

--- programming_elixir/first_steps/functions-3.exs
+++
@@ -1,3 +0,0 @@
-mathbuzz = fn
-  (n) -> fizzbuzz.(rem(n,3),rem(n,5),n)
-end
--- programming_elixir/first_steps/functions-4.exs
+++
@@ -1,9 +0,0 @@
-prefix = fn title ->
-  fn name ->
-    title <> " " <> name
-  end
-end
-
-mrs = prefix.("Mrs")
-IO.puts mrs.("Smith")
-IO.puts prefix.("Elixir").("Rocks")
